e0fd42ef1b57bf8dbb0f4459f7a5de8ecdff7ccc
Modhura <32320483+ModhuraDas@users.noreply.github.com>
2018-11-02 12:52:59 +0530

    Add files via upload

---
+++ box.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+class Box
+{
+	int l,b,h;
+	public:
+	Box()
+	{}
+	Box(int x,int y,int z)
+	{	l=x;
+		b=y;
+		h=z;}
+	void operator++()
+	{	l++;
+		b++;
+		h++;}
+
+		//cout<<l<<b<<h;}
+	Box operator+(Box m)
+	{	l=l+m.l;
+		b=b+m.b;
+		h=h+m.h;
+		return Box(l,b,h);}
+	void display()
+	{	cout<<"\n\nLenth of box : "<<l;
+		cout<<"\nBreadth of box : "<<b;
+		cout<<"\nHeight of box : "<<h<<endl<<endl;}
+};
+
+
+int main()
+{	int m,n,p;
+	cout<<"\nEnter the length:"<<endl;
+	cin>>m;
+
+	cout<<"\nEnter the breadth:"<<endl;
+	cin>>n;
+
+	cout<<"\nEnter the height:"<<endl;
+	cin>>p;
+
+	Box b1(m,n,p),b2(m+1,n+1,p+1),b3(0,0,0);
+	b1.display();
+	b2.display();
+	++b1;
+	++b2;
+	b3=b1+b2;
+	cout<<"\n\nAfter adding corrosponding data members of two Box class objects :";
+	b3.display();
+}
+
